edd1e0be8d46c489bb0e23f31907bf7f7dce0df6
Igor Serebryany <igor47@moomers.org>
2023-08-28 09:40:22 -0700

    read request headers and body

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use std::env;
+use std::{env, io};
+use std::io::prelude::*;
 
 #[derive(Debug)]
 struct Header {
-    key: &'static str,
-    value: &'static str,
+    key: String,
+    value: String,
+}
+
+#[derive(Debug)]
+struct Body {
+    content_type: String,
+    content_length: u64,
+    data: String,
 }
 
 #[derive(Debug)]
@@ -11,23 +19,61 @@ struct Request {
     method: String,
     uri: String,
     headers: Vec<Header>,
-    body: String,
+    body: Option<Body>,
 }
 
 impl Request {
-    fn from_env() -> Self {
-        Self {
-            method: {
-                env::var("REQUEST_METHOD").unwrap()
-            },
-            uri: {
-                env::var("REQUEST_URI").unwrap()
-            },
+    fn from_env() -> Result<Request, &'static str> {
+        let method = match env::var("REQUEST_METHOD") {
+            Ok(method) => method,
+            Err(_) => return Err("REQUEST_METHOD not set"),
+        };
+
+        let uri = match env::var("REQUEST_URI") {
+            Ok(uri) => uri,
+            Err(_) => return Err("REQUEST_URI not set"),
+        };
+
+        Ok(Request {
+            method,
+            uri,
             headers: {
-                vec![]
+                let mut headers = Vec::new();
+                for (key, value) in env::vars() {
+                    if key.starts_with("HTTP_") {
+                        let key = &key[5..];
+                        let key = key.replace("_", "-");
+                        let key = key.to_lowercase();
+                        headers.push(Header { key, value });
+                    }
+                };
+
+                headers
             },
-            body: String::new(),
-        }
+            body: {
+                match env::var("CONTENT_LENGTH") {
+                    Ok(content_length) => {
+                        let content_length = match content_length.parse::<u64>() {
+                            Ok(content_length) => content_length,
+                            Err(_) => return Err("Invalid CONTENT_LENGTH"),
+                        };
+
+                        let content_type = match env::var("CONTENT_TYPE") {
+                            Ok(content_type) => content_type,
+                            Err(_) => return Err("CONTENT_TYPE not set"),
+                        };
+
+                        let mut data = String::new();
+                        if let Err(_) = io::stdin().read_to_string(&mut data) {
+                            return Err("Failed to read request body");
+                        }
+
+                        Some(Body { content_type, content_length, data })
+                    },
+                    Err(_) => None,
+                }
+            }
+        })
     }
 }
 
@@ -35,6 +81,22 @@ struct Response {
     status: u16,
     headers: Vec<Header>,
     body: String,
+}
+
+impl Response {
+    fn new() -> Response {
+        let mut response = Response {
+            status: 200,
+            headers: Vec::new(),
+            body: String::new(),
+        };
+        response.add_header("SMRS-Version", "0.0.1");
+        response
+    }
+
+    fn add_header(&mut self, key: &str, value: &str) {
+        self.headers.push(Header { key: key.to_string(), value: value.to_string() });
+    }
 }
 
 fn respond(response: &Response) {
@@ -46,7 +108,7 @@ fn respond(response: &Response) {
 }
 
 fn print_env(request: &Request, response: &mut Response) {
-    response.headers.push(Header { key: "Content-Type", value: "text/plain" });
+    response.add_header("Content-Type", "text/plain");
 
     for (key, value) in env::vars() {
         response.body.push_str(&format!("{0}: {1}\n", key, value));
@@ -56,11 +118,16 @@ fn print_env(request: &Request, response: &mut Response) {
 }
 
 fn main() {
+    let mut response = Response::new();
     let request = Request::from_env();
-    let mut response = Response {
-        status: 200,
-        headers: vec![Header { key: "SMRS-Version", value: "0.0.1" }],
-        body: String::new(),
+    let request = match request {
+        Ok(request) => request,
+        Err(err) => {
+            response.status = 400;
+            response.body = String::from(format!("Invalid request: {}", err));
+            respond(&response);
+            return;
+        }
     };
 
     print_env(&request, &mut response);
